26dd32b9d1dfafc2b8eb4d38d7ce8d9548ca5f02
ZengXinXian <t110360124@ntut.org.tw>
2023-11-02 12:03:23 +0800

    HW1102

---
+++ src/P07/HW1102.java
@@ -0,0 +1,27 @@
+package P07;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.*;
+
+public class HW1102 {
+	
+		public static void main(String[] args) throws IOException
+		{
+			
+			System.out.println("請輸入整數");
+			BufferedReader br=
+					new BufferedReader(new InputStreamReader(System.in));
+			
+			int num=Integer.parseInt(br.readLine());
+			if(num==1) 
+			{
+				System.out.println("輸入的是1");
+				System.out.println("選擇的是1");	
+			}
+			System.out.println("結束處理");
+		}
+	
+	
+
+}
---
+++ src/P26/P26.java
@@ -0,0 +1,20 @@
+package P26;
+
+import java.io.IOException;
+
+public class P26 {
+
+	public static void main(String[] args) 
+	{
+		int i=1;
+		while(i<=5)
+		{
+			System.out.println("第" +i+ "次i的迴圈");
+			i++;
+		}
+		
+		System.out.println("迴圈結束");
+		
+		
+	}
+}
